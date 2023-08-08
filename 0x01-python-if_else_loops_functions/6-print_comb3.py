e22ea7c9a7bcc07f309f8b50cb869d1d6d9e3eb4
abdo <abdoraja2005@gmail.com>
2023-08-08 11:10:31 +0100

    task from 2 to 8

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for c in range(10):
+    for d in range((c+1), 10):
+        if (c == 8 and d == 9):
+            print("89")
+        else:
+            print("{}".format(c), end="")
+            print("{},".format(d), end=" ")
