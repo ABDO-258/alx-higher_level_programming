5988201878cc8c1e66fed1069129e7fc0dac56cd
abdo <abdoraja2005@gmail.com>
2023-08-11 00:09:17 +0100

    edge cass of null

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,24 +11,25 @@ int check_cycle(listint_t *list)
 	listint_t *ptr1, *ptr2;
 
 	ptr1 = list;
-	ptr2 = list->next;
-
 	if (list != NULL)
 	{
-		while (ptr1 != NULL && ptr2->next != NULL)
+		ptr2 = list->next;
+		while (ptr1 != NULL && ptr2 != NULL)
 		{
-			/*
-			*printf("------------\n");
-			*printf("ptr1 = %i\n", ptr1->n);
-			*printf("ptr2 = %i\n", ptr2->n);
-			*printf("------------\n");
-			*/
+			
+			printf("------------\n");
+			printf("ptr1 = %i\n", ptr1->n);
+			printf("ptr2 = %i\n", ptr2->n);
+			printf("------------\n");
+			
 			if (ptr1 == ptr2)
 				return (1);
 			ptr1 = ptr1->next;
 			ptr2 = ptr2->next->next;
 		}
 	}
-		return (0);
+	else
+		ptr2 = NULL;
+	return (0);
 
 }
