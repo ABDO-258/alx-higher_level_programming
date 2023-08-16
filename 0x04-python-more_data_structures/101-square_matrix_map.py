28df517a406108c7290de56f7d13538d8c7a1166
abdo <abdoraja2005@gmail.com>
2023-08-16 14:25:17 +0100

    3 line requirment

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda y: y * y, row)), matrix))
-    return new_matrix
+    return list(map(lambda row: list(map(lambda y: y * y, row)), matrix))
