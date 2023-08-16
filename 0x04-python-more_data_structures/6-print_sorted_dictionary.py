8bdb30f2c0daf865b8a45057268b7b226e9eac72
abdo <abdoraja2005@gmail.com>
2023-08-16 12:50:09 +0100

    add a space after : in format

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new_dictionary = sorted(a_dictionary.items())
     for key, value in new_dictionary:
-        print(f"{key}:{value}")
+        print("{}: {}".format(key, value))
