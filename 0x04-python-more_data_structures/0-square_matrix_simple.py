ea7647408d0249004fe2aed980f82efa25cd3c1e
abdo <abdoraja2005@gmail.com>
2023-08-16 11:29:37 +0100

    task0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i, row in enumerate(matrix):
+        new_row = []
+        for j, num in enumerate(row):
+            new_row.append(num * num) 
+        new_matrix.append(new_row)
+    return new_matrix
+
+
+
+
+matrix = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
