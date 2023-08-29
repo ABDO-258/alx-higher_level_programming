1f585d2b078ce4ed3e6701d38c120c3756f05d19
abdo <abdoraja2005@gmail.com>
2023-08-29 14:52:47 +0100

    task [1]

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' Squre class to make a square '''
+
+
+class Square:
+    ''' create an empty class'''
+    def __init__(self, size):
+        '''
+        intialize the class
+
+        size = size of a square
+
+        '''
+        self._size = size
