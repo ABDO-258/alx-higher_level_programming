cb41103722957897420656e3353754ede28db907
abdo <abdoraja2005@gmail.com>
2023-08-29 23:17:49 +0100

    task [6]fix pycode style + add some edge casse to position setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not  tuple:
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (len(value) != 2):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0]) is not int) or (type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
