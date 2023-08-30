7d262b091d36c09ef4ec97de60f3690c4a316ac8
abdo <abdoraja2005@gmail.com>
2023-08-30 11:04:56 +0100

    try task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,8 +14,8 @@ class Square:
         size must be bigger than 0
 
         '''
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -36,7 +36,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple:
+        if (type(value) is not tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -44,7 +44,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (value[0] < 0) or (value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """ methode to calculate the area of a square"""
