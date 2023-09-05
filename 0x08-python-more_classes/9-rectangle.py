5a201c3f1782544af78607f2c345427d243ab306
abdo <abdoraja2005@gmail.com>
2023-09-05 11:56:32 +0100

    task[9] change name of variable in init and fix error msg for height sitter

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ intialize  a rectangle"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
@@ -36,9 +36,9 @@ class Rectangle:
     def height(self, value):
         """ getting the height of a rectangle"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
