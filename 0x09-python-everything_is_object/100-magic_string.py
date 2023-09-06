9613a0c48c1d96e8c104b1fa7ac8f5fdfe9084e4
abdo <abdoraja2005@gmail.com>
2023-09-06 13:54:30 +0100

    fix pycodestyle error

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l.append("BestSchool")
-    return ", ".joint(l)
+def magic_string(new_list=[]):
+    new_list.append("BestSchool")
+    return ", ".join(new_list)
