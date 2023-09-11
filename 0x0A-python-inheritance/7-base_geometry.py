c0c36fac47e353a520a783502f7be04a13180be0
abdo <abdoraja2005@gmail.com>
2023-09-11 19:36:19 +0100

    task7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" modul of empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+    def area(self):
+        """ method that raise an exeption """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validation of value as integer """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
