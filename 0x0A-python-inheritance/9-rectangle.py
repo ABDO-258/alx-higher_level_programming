841c49192f6c15cb0b2a7b84f7b4f2b1473d90a8
abdo <abdoraja2005@gmail.com>
2023-09-11 21:46:41 +0100

    task9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" modul  class Rectangle  """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle subclass of BaseGeometry """
+    def __init__(self, width, height):
+        """Intialize a Rectangle """
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
