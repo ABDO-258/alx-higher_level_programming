beb883b534cacdd251772c9e9c434018cf770557
abdo <abdoraja2005@gmail.com>
2023-09-11 18:48:13 +0100

    task4 add permistion to all file

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" modul of a function that returns True if the object is an instance
+      of a class that inherited (directly or indirectly) from the specified
+        class ; otherwise False.
+    """
+
+
+def inherits_from(obj, a_class):
+    """ function that check the inheritance is derict or inderict """
+    if issubclass(type(obj), a_class):
+        if type(obj) is not a_class:
+            return True
+    else:
+        return False
