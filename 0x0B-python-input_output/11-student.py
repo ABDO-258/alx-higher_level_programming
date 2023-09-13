052720ce741d8b529fff465ec66de76e41dc09db
abdo <abdoraja2005@gmail.com>
2023-09-13 03:16:41 +0100

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a class Student that defines a student by:
+first_name
+last_name
+age """
+
+
+class Student:
+    """ a class Student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dictionary description with simple
+        data structure (list, dictionary, string, integer and boolean)
+        for JSON serialization of an object """
+        if type(attrs) is list:
+            if all(type(element) is str for element in attrs):
+                return {key: self.__dict__[key]
+                        for key in attrs if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__.update(json)
