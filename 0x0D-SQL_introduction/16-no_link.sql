cf3e3896bebd8e9c1f8b60a21f826fa14cd7315f
abdo <abdoraja2005@gmail.com>
2023-10-18 15:58:28 +0100

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,3 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- new value for test
---INSERT INTO second_table (id, name, score) VALUES(5, "Aria", 18);
---INSERT INTO second_table (id, name, score) VALUES(6, "Aria", 12);
---INSERT INTO second_table (id, score) VALUES(7, 14);
---INSERT INTO second_table (id, score) VALUES(8, 8);
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
