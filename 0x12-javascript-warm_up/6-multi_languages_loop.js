27f3b3d1b7dcc83fcf296b066174d4165dc9fc28
abdo <abdoraja2005@gmail.com>
2023-11-13 20:01:47 +0100

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let index;
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (index = 0; index < myVar.length; index++) {
+  console.log(myVar[index]);
+}
