e580f66a8a4ed088212f2a3a621cc58eb673f547
abdo <abdoraja2005@gmail.com>
2023-11-14 02:09:00 +0100

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function Factorial (n) {
+  if (n === 0) {
+    return (1);
+  } else {
+    return (n * Factorial(n - 1));
+  }
+}
+
+const myArg1 = parseInt(process.argv[2]);
+const result = Factorial(myArg1);
+console.log(result);
