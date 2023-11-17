72b948c44c7c7ae36a03762354baddb4f62bc675
abdo <abdoraja2005@gmail.com>
2023-11-17 18:41:35 +0100

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+
+def list_all_states(username, password, database, searched):
+    """ function that lists all states from the database hbtn_0e_0_usa"""
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE BINARY name = '{}'".format(searched)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    searched = sys.argv[4]
+    list_all_states(username, password, database, searched)
