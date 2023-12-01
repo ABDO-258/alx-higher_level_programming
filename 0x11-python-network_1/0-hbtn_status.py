816827a67a6e7ca3760ecacbc8c1a2e5a472ccc5
abdo <abdoraja2005@gmail.com>
2023-12-01 14:30:19 +0100

    task [0]fix pycodestyle typo errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         data = response.read()
         print("Body response:")
-        print(f"\t- type {type(data)}")
-        print(f"\t- content {data}")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
         print(f"\t- utf8 content: {data.decode(encoding='utf-8')}")
-        
