75b0408b495596a294cb585e70a4c45945fcabde
abdo <abdoraja2005@gmail.com>
2023-12-02 22:12:36 +0100

    task [9]

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  script that uses the GitHub API to display the user ID
+using Basic Authentication with a personal access token """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(username, password))
+    if r.status_code == 200:
+        data = r.json()
+        user_id = data["id"]
+        print(user_id)
+    else:
+        print(None)
+   
