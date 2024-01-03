811c53c1d6b6e01d9b54b839d88395fde0750205
abdo258 <abdoraja2005@gmail.com>
2024-01-03 23:19:34 +0100

    task[2]

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Check the arguments are 3
+if (process.argv.length !== 3) {
+  console.error('Usage: node 2-statuscode.js <URL>');
+  process.exit(1);
+}
+
+// Get the url from command-line arguments
+const url = process.argv[2];
+
+//make a request to url
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
