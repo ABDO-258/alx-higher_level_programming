6491d2510d739cf3763405aa00bd1198149dcca5
abdo258 <abdoraja2005@gmail.com>
2024-01-04 21:42:50 +0100

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+// Check the arguments are 3
+if (process.argv.length !== 4) {
+  console.error('Usage: node 5-request_store.js <URL> <file path>');
+  process.exit(1);
+}
+
+// Get the url from command-line arguments
+const WebUrl = process.argv[2];
+console.log(WebUrl);
+// Get the file path
+const filePath = process.argv[3];
+console.log(filePath);
+// make a request to url
+request.get(WebUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    // write the content to the file in utf-8
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
